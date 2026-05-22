71453a54a5a999b01d4f858c4cc5c0bbc380501d
Marcella <marcella.pinho.brandao4@gmail.com>
2026-05-22 19:43:47 -0300

    adicionar geração de token JWT

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,7 +1,19 @@
 from passlib.context import CryptContext
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+
+import os
+
+
+load_dotenv()
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 
 def get_hash_password(password: str):
@@ -10,3 +22,12 @@ def get_hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+# Recebe os dados do usuário, adiciona tempo de expiração e retorna um token JWT assinado
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
